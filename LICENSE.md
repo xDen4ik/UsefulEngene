abcc0ab968ba501d91e48fdd5e4730caedeab25e
Denis Pacha <cool.den0@yandex.ru>
2023-08-28 23:52:28 +0300

    fix date

--- LICENSE.md
+++ LICENSE.md
@@ -1,2 +1,2 @@
-<h1>Copyright © 2022 by Denis Pacha (<a>https://www.linkedin.com/in/denis-pacha</a>)</h1>
+<h1>Copyright © 2023 by Denis Pacha (<a>https://www.linkedin.com/in/denis-pacha</a>)</h1>
 <h2>All rights reserved<h2>
--- backend/views/layouts/content.php
+++ backend/views/layouts/content.php
@@ -24,7 +24,7 @@ use dmstr\widgets\Alert;
 	<div class="pull-right hidden-xs">
 		<b>Version</b> 1.0
 	</div>
-	<strong>Copyright &copy; 2021 <a href="https://vk.com/dennnnnnnnnnn">Denis Pacha</a>.</strong> All rights
+	<strong>Copyright &copy; 2023 <a href="https://vk.com/dennnnnnnnnnn">Denis Pacha</a>.</strong> All rights
 	reserved.
 </footer>
 
